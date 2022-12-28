29265a06d8e1b112ebef51806a00847c3acee3c1
Gaspar Ibañez <gaspar.ibanez@fiserv.com>
2022-12-28 17:22:20 -0300

    Se añaden y ordenan los archivos correspondiente para la creacion del servidor con express.

---
+++ app.js
@@ -0,0 +1,23 @@
+import path, { dirname } from "path"
+import { fileURLToPath } from 'url';
+import express from "express";
+// Routes
+import indexRouter from './src/routes/indexRouter.js';
+
+//
+const __dirname = dirname(fileURLToPath(import.meta.url));
+console.log(__dirname);
+//Server 
+
+const app = express()
+const port = process.env.PORT || 3000;
+
+// Server Config
+app.use(express.static(path.resolve(__dirname,'../public')));
+app.use("/",indexRouter);
+app.use("/*",(req,res,next)=>{
+    res.send("Error 404! File Not Found")
+});
+
+// Server Init
+app.listen(port, () => console.log(`Server escuchando en puerto ${port}`));
