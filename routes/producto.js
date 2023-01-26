ca3ce2b58d58dd1e19eef8858d47a3315a21cbc9
Brandon <brandon.11.12.97@gmail.com>
2023-01-25 22:33:09 -0300

    se creo el controller de producto

---
+++ routes/producto.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+// ************ Controller Require ************
+const productsController = require('../controllers/productoController');
+
+
+
+module.exports = router;
